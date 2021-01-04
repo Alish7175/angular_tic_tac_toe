3dcd95c9d66df856544ac5e97739a4bdcf1967b5
alish <alishmadhukar75@gmail.com>
2021-01-05 00:38:08 +0530

    erroe fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,24 @@ import { AppComponent } from './app.component';
 import { IconComponent } from './components/icon/icon.component';
 
 //font-awesome
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-//toastr
-import {  BrowserAnimationsModule  } from "@angular/platform-browser/animations";
-import { ToastrModule } from "ngx-toastr";
+//Toaster
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IconComponent
-  ],
+  declarations: [AppComponent, IconComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    BrowserAnimationsModule,
-    ToastrModule
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/icon/icon.component.spec.ts
+++ src/app/components/icon/icon.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconComponent } from './icon.component';
 
@@ -6,12 +6,12 @@ describe('IconComponent', () => {
   let component: IconComponent;
   let fixture: ComponentFixture<IconComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ IconComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IconComponent);
